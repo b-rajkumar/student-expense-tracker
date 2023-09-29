5dc13be0acbeee146722dabc5a231b414e172d5a
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-09-29 22:03:52 +0530

    Add confirm password section in sign up page

--- scripts/sign-up.js
+++ scripts/sign-up.js
@@ -1,6 +1,16 @@
 const submitSignUpForm = () => {
   const name = document.querySelector("#name-text-box").value;
   const password = document.querySelector("#password-text-box").value;
+  const confirmPassword = document.querySelector(
+    "#confirm-password-text-box"
+  ).value;
+
+  if (password !== confirmPassword) {
+    const errorContainer = document.querySelector("#error-box");
+    errorContainer.classList.remove("hide");
+    return;
+  }
+
   const users = JSON.parse(localStorage.getItem("users"));
 
   if (name in users) return alert("Username Already Exists");
